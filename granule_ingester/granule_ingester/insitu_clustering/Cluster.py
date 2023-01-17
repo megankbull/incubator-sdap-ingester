e7ec8ec0c01f08f5e1cddb34aa7f4b7ceb6498cc
rileykk <rileykk@jpl.nasa.gov>
2023-01-17 12:26:11 -0800

    Cluster detect base class detect loop

--- granule_ingester/granule_ingester/insitu_clustering/Cluster.py
+++ granule_ingester/granule_ingester/insitu_clustering/Cluster.py
@@ -37,14 +37,22 @@ class ClusterSearch(ABC):
         self._args: Namespace = args
         self._solr: pysolr.Solr = solr
 
+        self._rmq_host = args.rabbitmq_host
+        self._rmq_user = args.rabbitmq_username
+        self._rmq_password = args.rabbitmq_password
+        self._rmq_queue = args.insitu_rmq
+
+        self._rmq = MessagePublisher(self._rmq_host, self._rmq_user, self._rmq_password, self._rmq_queue)
+
     # Returns uuid list & dataset_s
     @abstractmethod
     def _detect_clusters(self) -> Tuple[List[str], str]:
         pass
 
     # method: all = empty stage, isolated, max
+    # returns list of cluster tuples
     @abstractmethod
-    def flush(self, method, **kwargs):
+    def flush(self, method, **kwargs) -> List[Tuple[List[str], str]]:
         pass
 
     @staticmethod
@@ -58,4 +66,15 @@ class ClusterSearch(ABC):
         return dump(msg_dict, Dumper=Dumper, sort_keys=False)
 
     def start_detecting(self):
-        pass
+        logger.info('Beginning cluster detection')
+
+        for uuids, dataset in self._detect_clusters():
+            logger.info(f'Found cluster of {len(uuids)} observations for dataset {dataset}')
+            logger.debug('Building rmq message')
+            message = ClusterSearch._build_message(uuids, dataset)
+
+            logger.debug('Publishing to rmq')
+            async with self._rmq:
+                await self._rmq.publish_message(message)
+
+            logger.debug('Message published')
